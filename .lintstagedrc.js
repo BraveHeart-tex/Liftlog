2b8c1edacb1c665fdd34ea6b4722e20f7235c274
Bora Karaca <bora.karaca@ogr.iu.edu.tr>
2026-04-10 00:11:48 +0300

    chore: move lint-staged config into package.json

--- .lintstagedrc.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  '**/*.{ts,tsx,js}': stagedFiles => [
-    `pnpm exec eslint --fix ${stagedFiles.join(' ')}`,
-    `pnpm exec prettier --write ${stagedFiles.join(' ')}`
-  ],
-  '**/*.{md,json}': stagedFiles =>
-    `pnpm exec prettier --write ${stagedFiles.join(' ')}`
-};
