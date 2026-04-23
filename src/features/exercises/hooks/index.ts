faf6a52dd23fa2e0eea0a1159a64c6b37d1ffb03
Bora Karaca <bora.karaca@ogr.iu.edu.tr>
2026-04-23 13:37:35 +0300

    feat(exercises): centralize exercise database logic

--- src/features/exercises/hooks/index.ts
+++ src/features/exercises/hooks/index.ts
@@ -1,3 +1,4 @@
+export { useExerciseActions } from './use-exercise-actions';
 export { useExerciseDetail } from './use-exercise-detail';
 export { useExercises } from './use-exercises';
 export { useExercisesScreen } from './use-exercises-screen';
